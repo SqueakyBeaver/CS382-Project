d85defdc457a45171a348baa9dd5219adc4c9001
Beaverr <beaver@transgender.army>
2025-03-26 11:53:31 -0500

    Add lexical analyzer skeleton

---
+++ lex/lex.h
@@ -0,0 +1,23 @@
+// This header file only exists to de-clutter main.c
+#include <stdio.h>
+
+
+
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
+
+#define LETTER 0
+#define DIGIT 1
+#define UNKNOWN 99
+
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
---
+++ lex/main.c
@@ -0,0 +1,24 @@
+#include <ctype.h>
+#include <stdio.h>
+
+#include "lex.h"
+
+// Global variables
+int charClass;
+char lexeme[100];
+char nextChar;
+int lexLen;
+int nextToken;
+FILE *in_fp;
+
+int main() {}
+
+int lookup(char ch) {}
+
+void addChar() {}
+
+void getChar() {}
+
+void getNonBlank() {}
+
+int lex() {}
