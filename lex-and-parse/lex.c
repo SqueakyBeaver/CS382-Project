9ebc4669f80ea6347c428557b16c5fa064ac7f99
Beaverr <squeaky.beaver4133@protonmail.com>
2025-03-30 18:19:01 -0500

    Code cleanup

--- lex-and-parse/lex.c
+++ lex-and-parse/lex.c
@@ -11,59 +11,55 @@ multiplicative operators | *, /
 */
 
 int lookup(char ch) {
+    addChar();
     // lookup the token for characters that aren't alphanumeric
     switch (ch) {
-        case '(':
-            addChar();
-            nextToken = LEFT_PAREN;
-            break;
-        case ')':
-            addChar();
-            nextToken = RIGHT_PAREN;
-            break;
-        case '+':
-            addChar();
-            nextToken = ADD_OP;
-            break;
-        case '-':
-            addChar();
-            nextToken = SUB_OP;
-            break;
-        case '*':
-            addChar();
-            nextToken = MULT_OP;
-            break;
-        case '/':
-            addChar();
-            nextToken = DIV_OP;
-            break;
-        default:
-            addChar();
-            nextToken = EOF;
-            break;
+    case '(':
+        nextToken = LEFT_PAREN;
+        break;
+    case ')':
+        nextToken = RIGHT_PAREN;
+        break;
+    case '+':
+        nextToken = ADD_OP;
+        break;
+    case '-':
+        nextToken = SUB_OP;
+        break;
+    case '*':
+        nextToken = MULT_OP;
+        break;
+    case '/':
+        nextToken = DIV_OP;
+        break;
+    default:
+        nextToken = EOF;
+        break;
     }
     return nextToken;
 }
 
 void addChar() {
     // Add nextChar to lexeme array
-    if (lexLen <= 254) {
-        lexeme[lexLen++] = nextChar;
-        lexeme[lexLen] = 0;
-    }
-    else {
+    if (lexLen > 254) {
         printf("Error - lexeme is too long \n");
+        return;
     }
+
+    lexeme[lexLen++] = nextChar;
+    lexeme[lexLen] = 0;
 }
 
 void getChar() {
     // Reassign nextChar
     // find nextChar's character class and store that
     nextChar = getc(in_fp);
-    
+
     if (nextChar == EOF) {
         charClass = EOF;
+        return;
     }
+
     if (isalpha(nextChar)) {
         charClass = LETTER;
     } else if (isdigit(nextChar)) {
@@ -75,52 +71,56 @@ void getChar() {
 
 void getNonBlank() {
     // calls getChar() until a non-whitespace char is returned
-    while(isspace(nextChar)) {
+    while (isspace(nextChar))
         getChar();
-    }
 }
 
 int lex() {
     lexLen = 0;
     getNonBlank();
-    switch (charClass) {
-        // parse identifiers
-        case LETTER:
-            addChar();
-            getChar();
-            while (charClass == LETTER || charClass == DIGIT) {
-                addChar();
-                getChar();
-            }
-            nextToken = IDENT;
-            break;
-        
-        // parse int literals
-        case DIGIT:
-            addChar();
-            getChar();
-            while (charClass == DIGIT) {
-                addChar();
-                getChar();
-            }
-            nextToken = INT_LIT;
-            break;
-        
-        // parentheses and operators
-        case UNKNOWN:
-            lookup(nextChar);
-            getChar();
-            break;
 
-        // EOF
-        case EOF:
-            nextToken = EOF;
-            lexeme[0] = 'E';
-            lexeme[1] = 'O';
-            lexeme[2] = 'F';
-            lexeme[3] = 0;
-            break;
+    switch (charClass) {
+    // parse identifiers
+    case LETTER:
+        addChar();
+        getChar();
+
+        while (charClass == LETTER || charClass == DIGIT) {
+            addChar();
+            getChar();
+        }
+
+        nextToken = IDENT;
+        break;
+
+    // parse int literals
+    case DIGIT:
+        addChar();
+        getChar();
+
+        while (charClass == DIGIT) {
+            addChar();
+            getChar();
+        }
+
+        nextToken = INT_LIT;
+        break;
+
+    // parentheses and operators
+    case UNKNOWN:
+        lookup(nextChar);
+        getChar();
+        break;
+
+    // EOF
+    case EOF:
+        nextToken = EOF;
+        lexeme[0] = 'E';
+        lexeme[1] = 'O';
+        lexeme[2] = 'F';
+        lexeme[3] = 0;
+        break;
     }
-    // printf("Next token is: %d, Next lexeme is %s\n", nextToken, lexeme);
+
     return nextToken;
 }
